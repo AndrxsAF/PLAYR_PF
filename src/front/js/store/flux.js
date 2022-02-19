2b9ea32841f0620d122b7e021adeee454e8f8e82
AndrxsAF <andrxsaf@gmail.com>
2022-02-19 12:21:14 +0100

    debugging repo

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,46 +1,19 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			topAnime: [],
+			anime: {},
+			copyTopAnime: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			setTopAnime: (topAnimeList) => {
+				setStore({topAnime: topAnimeList });
 			},
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
+			setAnime : (anime) => {
+				setStore({anime: anime});
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			setCopyTopAnime: (copyTopAnime) => {
+				setStore({copyTopAnime: copyTopAnime})
 			}
 		}
 	};
