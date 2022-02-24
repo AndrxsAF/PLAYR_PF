ec5459fd2fc57d9e8344db5d4cafc060a4ab00bd
AndrxsAF <andrxsaf@gmail.com>
2022-02-24 22:06:06 +0000

    FEAT NAVBAR RESPONSIVE DESIGN

--- src/front/js/Layout/Navbar/Navbar.jsx
+++ src/front/js/Layout/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState, useContext, useEffect} from "react";
 import {  NavLink } from "react-router-dom";
 import { Context } from "../../store/appContext"
 import Logo from "../../../img/logo-proyecto.png"
+import PhoneLogo from "../../../img/small-logo.png"
 import Rigo from "../../../img/rigo-baby.jpg"
 import "./Navbar.css"
 
@@ -9,32 +10,88 @@ export const Navbar = () => {
 
   const { store, actions } = useContext(Context)
 	const [addClass, setClass] = useState("visually-hidden")
+  const [addClassSearch, setClassSearch] = useState("visually-hidden")
+  const [addClassNotifications, setClassNotifications] = useState("visually-hidden")
 
-	const classToggle = () => addClass == "d-block" ? setClass("visually-hidden") : setClass("d-block")
+  const classToggle = () => {
+    if (addClass == "d-block") {
+      setClass("visually-hidden")
+      setClassSearch("visually-hidden")
+      setClassNotifications("visually-hidden")
+    } else {
+      setClass("d-block")
+      setClassSearch("visually-hidden")
+      setClassNotifications("visually-hidden")
+    }
+  }
+
+	const searchClassToggle = () => {
+    if (addClassSearch == "d-block") {
+      setClass("visually-hidden")
+      setClassSearch("visually-hidden")
+      setClassNotifications("visually-hidden")
+    } else {
+      setClass("visually-hidden")
+      setClassSearch("d-block")
+      setClassNotifications("visually-hidden")
+    }
+  }
+
+  const notificationsClassToggle = () => {
+    if (addClassNotifications == "d-block") {
+      setClass("visually-hidden")
+      setClassSearch("visually-hidden")
+      setClassNotifications("visually-hidden")
+    } else {
+      setClass("visually-hidden")
+      setClassSearch("visually-hidden")
+      setClassNotifications("d-block")
+    }
+  }
  
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
       <img src={Logo} alt="PLAYR" className="logo ps-5"/>
+      <img src={PhoneLogo} alt="PLAYR" className="phone-logo"/>
+
       <div class="input-group search-bar">
-        <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1"/>
+        <input type="text" className="form-control search-form" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1"/>
       </div>
-      <div className="d-flex pe-5">
-        <div className="d-flex align-items-center me-3">
+
+      <div className="d-flex pe-5 user">
+        <div onClick={() => searchClassToggle()} className="me-3 icon d-flex align-items-center">
+        <img className="search" src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"/>
+        <ul className={"bg-light dropdown-menu search search-dropdown " + addClassSearch}>
+            <li className="px-3"><input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1"/></li>
+        </ul>
+        </div>
+        <div className="d-flex align-items-center me-3 icon">
           <img src="https://img.icons8.com/ios-filled/30/000000/plus-math.png"/>
-          <p className="m-0 ps-1">Nuevo</p>
+          <p className="m-0 ps-1 text-icon">Nuevo</p>
         </div>
-        <div className="d-flex align-items-center me-3">
+        <div onClick={() => notificationsClassToggle()} className="d-flex align-items-center me-3 icon">
           <img src="https://img.icons8.com/fluency-systems-regular/30/000000/star--v1.png"/>
-          <p className="m-0 ps-1">Notificaciones</p>
+          <p className="m-0 ps-1 text-icon">Notificaciones</p>
+          <ul className={"bg-light dropdown-menu user-menu " + addClassNotifications}>
+            <li><a className="dropdown-item" href="#">Notificación 1</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Notificación 2</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Notificación 3</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Notificación 4</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Notificación 5</a></li>
+          </ul>
         </div>
-        <div className="d-flex align-items-center me-3">
+        <div className="d-flex align-items-center me-3 icon">
           <img src="https://img.icons8.com/ios-filled/30/000000/hashtag.png"/>
-          <p className="m-0 ps-1">Explora</p>
+          <p className="m-0 ps-1 text-icon">Explora</p>
         </div>
         <div onClick={() => classToggle()} className="d-flex align-items-center justify-content-end">
-          <p className="m-0 pe-1 username">USUARIO</p>
+          <p className="m-0 pe-1 username text-icon">USUARIO</p>
           <img src={Rigo} alt="User" className="profile-pic"/>
-          <ul style={{left: "auto", top: "100%"}} className={"bg-light dropdown-menu " + addClass}>
+          <ul className={"bg-light dropdown-menu user-menu " + addClass}>
             <li><a className="dropdown-item" href="#">Perfil</a></li>
             <li><hr className="dropdown-divider"/></li>
             <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
