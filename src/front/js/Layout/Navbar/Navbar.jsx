4e5c817d0ff15f9064e0c4af8e417e6b589c432a
AndrxsAF <andrxsaf@gmail.com>
2022-02-24 21:10:20 +0000

    FEAT LOGO AND NAVBAR

--- src/front/js/Layout/Navbar/Navbar.jsx
+++ src/front/js/Layout/Navbar/Navbar.jsx
@@ -1,63 +1,46 @@
 import React, {useState, useContext, useEffect} from "react";
 import {  NavLink } from "react-router-dom";
 import { Context } from "../../store/appContext"
+import Logo from "../../../img/logo-proyecto.png"
+import Rigo from "../../../img/rigo-baby.jpg"
+import "./Navbar.css"
 
 export const Navbar = () => {
-  const {store, actions} = useContext(Context)
 
-  const handelChange = (e) => {
-    const search = e.target.value;
-    console.log(search)
-    if(search === ""){
-      actions.setTopAnime(store.copyTopAnime);
-    }else{
-      const newListFilterTopAnime = store.copyTopAnime.filter( anime =>  {
-        const tilteAnime = anime.title.toLowerCase();
-        if(tilteAnime.indexOf(search.toLowerCase()) >= 0){
-          return anime;
-        }
-      });
-      console.log({newListFilterTopAnime})
-      actions.setTopAnime(newListFilterTopAnime)
-    }
-  }
-  console.log(store.copyTopAnime)
+  const { store, actions } = useContext(Context)
+	const [addClass, setClass] = useState("visually-hidden")
+
+	const classToggle = () => addClass == "d-block" ? setClass("visually-hidden") : setClass("d-block")
+ 
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Anime 4GeeksAcademy</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Top Anime</NavLink>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+      <img src={Logo} alt="PLAYR" className="logo ps-5"/>
+      <div class="input-group search-bar">
+        <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1"/>
+      </div>
+      <div className="d-flex pe-5">
+        <div className="d-flex align-items-center me-3">
+          <img src="https://img.icons8.com/ios-filled/30/000000/plus-math.png"/>
+          <p className="m-0 ps-1">Nuevo</p>
+        </div>
+        <div className="d-flex align-items-center me-3">
+          <img src="https://img.icons8.com/fluency-systems-regular/30/000000/star--v1.png"/>
+          <p className="m-0 ps-1">Notificaciones</p>
+        </div>
+        <div className="d-flex align-items-center me-3">
+          <img src="https://img.icons8.com/ios-filled/30/000000/hashtag.png"/>
+          <p className="m-0 ps-1">Explora</p>
+        </div>
+        <div onClick={() => classToggle()} className="d-flex align-items-center justify-content-end">
+          <p className="m-0 pe-1 username">USUARIO</p>
+          <img src={Rigo} alt="User" className="profile-pic"/>
+          <ul style={{left: "auto", top: "100%"}} className={"bg-light dropdown-menu " + addClass}>
+            <li><a className="dropdown-item" href="#">Perfil</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Cerrar Sesión</a></li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" onChange={handelChange}>
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        </div>
+      </div>
+    </nav>
 	);
 };
