54c6cef3aac4d38738ef3086444c1067882015c2
CAMFER27 <cinematografico27@gmail.com>
2022-02-26 00:06:52 +0000

    register al 95%

--- src/front/js/views/register/register.jsx
+++ src/front/js/views/register/register.jsx
@@ -5,7 +5,7 @@ import { Form } from "react-bootstrap";
 const Register = () => {
 	return (
 		<div className="Container0">
-			<div id="Container1" className="container-fluid wh-100 col align-self-center">
+			<div id="Container1" className="container-fluid col align-self-center">
 				<div id="Titulo" className="col-3 offset-4 d-flex justify-content-between">
 					<h1>PLAYR</h1>
 					<p>The Social <br />
@@ -16,73 +16,68 @@ const Register = () => {
 				<div className="col-3 offset-4">
 					<p>Registrate y adentrate en el mundo gaming.</p>
 				</div>
-				<container className="body1">
-					<div className="col-3 offset-4">
-						<form id="form" className="card card-body">
-							<div className="mb-3">
-								<input
-									type="email"
-									className="form-control"
-									id="exampleInputEmail1"
-									aria-describedby="emailHelp"
-									placeholder="Ingresa tu correo electronico."
-								/>
-								<div id="emailHelp" className="form-text"></div>
-							</div>
-							<div className="mb-3">
-								<input
-									type="password"
-									className="form-control"
-									id="exampleInputPassword1"
-									placeholder="Ingresa tu usuario."
-								/>
-							</div>
-							<div className="mb-3">
-								<input
-									type="password"
-									className="form-control"
-									id="exampleInputPassword1"
-									placeholder="Ingresa tu contraseña."
-								/>
-							</div>
-							<div class="mb-3">
-								<input
-									type="password"
-									className="form-control"
-									id="exampleInputPassword1"
-									placeholder="Verifica tu contraseña."
-								/>
-							</div>
-							<div className="mb-3 form-check">
-								<input
-									type="checkbox"
-									className="form-check-input"
-									id="exampleCheck1"
-								/>
-								<label
-									className="form-check-label"
-									for="exampleCheck1">
-									Acepto los terminos y condiciones.
-								</label>
-							</div>
-							<div className="d-grid gap-2 col-6 mx-auto">
-								<button className="btn btn-primary" type="button">
-									Registrate
-								</button>
-							</div>
-						</form>
-					</div>
-								
-					<div id="Container2" className="col-6 offset-4">
-						<div className="card card-body col-6 offset-4">
-							<img src="img_girl.jpg" alt="Girl in a jacket" width="5000px" height="242px" />
+				<div id="body" className="col-3 offset-4">
+					<form id="form" className="card card-body">
+						<div className="mb-3">
+							<input
+								type="email"
+								className="form-control"
+								id="exampleInputEmail1"
+								aria-describedby="emailHelp"
+								placeholder="Ingresa tu correo electronico."
+							/>
+							<div id="emailHelp" className="form-text"></div>
 						</div>
-						<div className="card card-body col-6 offset-4">
-							<p class="text-center">¿Ya tienes una cuenta?</p>
-							<button type="button" class="btn btn-outline-primary">Ingresa en tu PLAYR</button>
+						<div className="mb-3">
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								placeholder="Ingresa tu usuario."
+							/>
 						</div>
+						<div className="mb-3">
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								placeholder="Ingresa tu contraseña."
+							/>
+						</div>
+						<div className="mb-3">
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								placeholder="Verifica tu contraseña."
+							/>
+						</div>
+						<div className="mb-3 form-check">
+							<input
+								type="checkbox"
+								className="form-check-input"
+								id="exampleCheck1"
+							/>
+							<label
+								className="form-check-label"
+								htmlFor="exampleCheck1">
+								Acepto los terminos y condiciones.
+							</label>
+						</div>
+						<div className="d-grid gap-2 col-3 offset-4">
+							<button className="btn btn-primary" type="button">
+								Registrate
+							</button>
+						</div>
+					</form>
+					
+				
+					<div className="card card-body">
+					<img src="img_girl.jpg" alt="Girl in a jacket" width="300px" height="200px" />
+					<p className="text-center">¿Ya tienes una cuenta?</p>
+					<button type="button" className="btn btn-outline-primary">Ingresa en tu PLAYR</button>
 					</div>
-				 </container>
+				</div>
 			</div>
 		</div>
 	);
