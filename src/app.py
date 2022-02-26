39d7c4deb7e9245777ddf3d198aec1e876b4d5cd
AndrxsAF <andrxsaf@gmail.com>
2022-02-26 13:46:15 +0000

    FEAT PUT AND GET ROUTE

--- src/app.py
+++ src/app.py
@@ -14,6 +14,10 @@ from api.admin import setup_admin
 from flask_jwt_extended import JWTManager
 #from models import Person
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
@@ -29,6 +33,9 @@ else:
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_KEY")
+app.config["CLOUD_NAME"] = os.environ.get("CLOUD_NAME")
+app.config["CLOUD_API_KEY"] = os.environ.get("CLOUD_API_KEY")
+app.config["CLOUD_API_SECRET"] = os.environ.get("CLOUD_API_SECRET")
 
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
@@ -43,6 +50,13 @@ setup_admin(app)
 
 app.register_blueprint(users, url_prefix="/api/main")
 # app.register_blueprint(posts, url_prefix="/api/post")
+
+cloudinary.config( 
+  cloud_name = app.config["CLOUD_NAME"], 
+  api_key = app.config["CLOUD_API_KEY"], 
+  api_secret = app.config["CLOUD_API_SECRET"],
+  secure = True
+)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
