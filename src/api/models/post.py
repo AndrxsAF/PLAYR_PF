abf6bc612f532e007036c438089b769c43abcc95
AndrxsAF <andrxsaf@gmail.com>
2022-03-01 04:32:51 +0000

    back/post model & endpoints, + models

--- src/api/models/post.py
+++ src/api/models/post.py
@@ -1,11 +1,19 @@
-from api.models.index import db
+from api.models.index import db, func
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    post = db.Column(db.String(160), nullable=False)
-    is_active = db.Column(db.Boolean, default=True)
+    img_url = db.Column(db.String(300))
+    description = db.Column(db.String(280))
+    game = db.Column(db.String(50))
+    console = db.Column(db.String(50))
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    tag1 = db.Column(db.String(50))
+    tag2 = db.Column(db.String(50))
+    tag3 = db.Column(db.String(50))
+    tag4 = db.Column(db.String(50))
+    tag5 = db.Column(db.String(50))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref='post')
+    user = db.relationship('User')
 
     def __repr__(self):
         return '<Post %r>' % self.id
@@ -13,6 +21,11 @@ class Post(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "post": self.post,
-            "user": self.user.serialize()
+            "img_url": self.img_url,
+            "description": self.description,
+            "game": self.game,
+            "console": self.console,
+            "date": self.date,
+            "tags": [self.tag1, self.tag2, self.tag3, self.tag4, self.tag5],
+            "user_id": self.user_id
         } 
---
+++ src/api/models/saved.py
@@ -0,0 +1,18 @@
+from api.models.index import db
+
+class Saved(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    user = db.relationship('User')
+    post = db.relationship('Post')
+
+    def __repr__(self):
+        return '<Saved %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        } 
